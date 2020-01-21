34e3f9d32f9ca28f95df40d55d0bff061fc1e9e1
Pierre Oblin <pierre.oblin@orange.com>
2020-01-21 17:15:28 +0000

    add k3s-images blob for airgap depl

--- addblob.sh
+++ addblob.sh
@@ -2,3 +2,6 @@
 wget https://github.com/rancher/k3s/releases/download/v1.17.0%2Bk3s.1/k3s
 bosh add-blob k3s k3s/k3s
 rm k3s
+
+wget https://github.com/rancher/k3s/releases/download/v1.17.0%2Bk3s.1/k3s-airgap-images-amd64.tar
+bosh add-blob k3s-images/k3s-airgap-images-amd64.tar k3s-airgap-images-amd64.tar
