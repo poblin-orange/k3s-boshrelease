ce59d5502bcaf1d59b6001e323231abf101269a7
Pierre Oblin <pierre.oblin@orange.com>
2020-01-26 13:53:27 +0000

    bump k3s v1.17.2-alpha3+k3s1

--- addblob.sh
+++ addblob.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-wget https://github.com/rancher/k3s/releases/download/v1.17.0%2Bk3s.1/k3s
+wget https://github.com/rancher/k3s/releases/download/v1.17.2-alpha3%2Bk3s1/k3s
 bosh add-blob k3s k3s/k3s
 rm k3s
 
-wget https://github.com/rancher/k3s/releases/download/v1.17.0%2Bk3s.1/k3s-airgap-images-amd64.tar
+wget https://github.com/rancher/k3s/releases/download/v1.17.2-alpha3%2Bk3s1/k3s-airgap-images-amd64.tar
 bosh add-blob k3s-images/k3s-airgap-images-amd64.tar k3s-airgap-images-amd64.tar
+rm k3s-airgap-images-amd64.tar
