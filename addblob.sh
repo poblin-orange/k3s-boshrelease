e617c3092b3704af288c5b85623fb3cff5899dc2
Pierre Oblin <pierre.oblin@orange.com>
2020-01-26 18:31:09 +0000

    bump blobs

--- addblob.sh
+++ addblob.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-wget https://github.com/rancher/k3s/releases/download/v1.17.2-alpha3%2Bk3s1/k3s
-bosh add-blob k3s k3s/k3s
-rm k3s
+wget https://github.com/rancher/k3s/releases/download/v1.17.2-alpha3%2Bk3s1/k3s -O src/k3s
+bosh add-blob src/k3s k3s/k3s
 
-wget https://github.com/rancher/k3s/releases/download/v1.17.2-alpha3%2Bk3s1/k3s-airgap-images-amd64.tar
-bosh add-blob k3s-images/k3s-airgap-images-amd64.tar k3s-airgap-images-amd64.tar
-rm k3s-airgap-images-amd64.tar
+wget https://github.com/rancher/k3s/releases/download/v1.17.2-alpha3%2Bk3s1/k3s-airgap-images-amd64.tar -O src/k3s-airgap-images-amd64.tar
+bosh add-blob src/k3s-airgap-images-amd64.tar k3s-images/k3s-airgap-images-amd64.tar
+
+wget https://github.com/derailed/k9s/releases/download/v0.13.6/k9s_0.13.6_Linux_x86_64.tar.gz -O src/k9s_0.13.6_Linux_x86_64.tar.gz
+cd src
+tar xfv k9s_0.13.6_Linux_x86_64.tar.gz
+cd ..
+bosh add-blob src/k9s k9s/k9s
+# rm src/k9s_0.13.6_Linux_x86_64.tar.gz
