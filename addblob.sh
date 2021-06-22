82a318f4b35f4762ca190823921238ae0324fc8c
Pierre Oblin <pierre.oblin@orange.com>
2021-06-22 17:27:54 +0000

    bump k9s 24.10

--- addblob.sh
+++ addblob.sh
@@ -4,7 +4,7 @@ bosh add-blob src/github.com/k3s-io/k3s/k3s k3s/k3s
 bosh add-blob src/github.com/k3s-io/k3s/k3s-airgap-images-amd64.tar k3s-images/k3s-airgap-images-amd64.tar
 
 pushd src/github.com/derailed/k9s/
-tar xfv ./k9s_Linux_x86_64.tar.gz
+tar xfv ./k9s_v0.24.10_Linux_x86_64.tar.gz
 popd
 bosh add-blob src/github.com/derailed/k9s/k9s k9s/k9s
 
